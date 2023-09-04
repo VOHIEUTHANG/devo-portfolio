ef2e08d47415f20c36ad9a8b0bd9f4ca3c8f4748
VOHIEUTHANG <hieuthang369@gmail.com>
2023-09-04 16:24:51 +0700

    update config-eslint-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,34 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
       files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script',
-      },
-    },
+        sourceType: 'script'
+      }
+    }
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react'],
   rules: {
-    semi: ['erorr', 'always'],
+    semi: ['error', 'always'],
+    'react/react-in-jsx-scope': 'off'
   },
+  'prettier/prettier': [
+    'error',
+    {},
+    {
+      usePrettierrc: true
+    }
+  ]
 };
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-
 const App = () => {
-  return <div className='App'>Hello 123</div>;
+  return <div className="App">Hello 123</div>;
 };
 
 export default App;
