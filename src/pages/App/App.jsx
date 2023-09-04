8464f1766157e3a7ae71855c3c76a2932c4ae819
VOHIEUTHANG <hieuthang369@gmail.com>
2023-09-04 13:33:15 +0700

    04092023 init project

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div className='App'>Hello 123</div>;
+};
+
+export default App;
---
+++ src/pages/Global/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './global.css';
+
+const GlobalPage = ({children}) => {
+  return (
+    <React.Fragment>{children}</React.Fragment>
+  )
+}
+
+export default GlobalPage
